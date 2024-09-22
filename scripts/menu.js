7ee14f050d541c3255dee8b2e7da54a834b9cd50
ElJuanys <84477702+ElJuanys@users.noreply.github.com>
2024-09-21 21:07:24 -0300

    first commit

---
+++ scripts/menu.js
@@ -0,0 +1,89 @@
+let url = "https://vps-3858808-x.dattaweb.com:8443/platos/traerPlatos?id=1"
+
+console.log("Probando fetch");
+
+fetch(url)
+
+.then(response => response.json())
+.then( data => {
+    const platos=data;
+    crearCategorias(platos);
+    mostrarPlatos(platos);
+
+
+
+    })
+.catch(error=> console.error("Ocurrió el siguiente error: ", error))
+
+
+function crearCategorias(platos){
+    const menu= document.getElementById("menu");
+    let categorias= ''; //para ir guardando las nuevas categorias
+    let guardaCategorias=[];
+    platos.forEach(plato => {
+        if(!guardaCategorias.some((catego)=> catego==plato.categoria)){
+            guardaCategorias.push(plato.categoria)
+            categorias+= `
+                <h2 class="categorias">${plato.categoria}</h2>   
+                <section id="${plato.categoria}" class="secciones">   
+                </section>`;
+        }
+    });
+    menu.innerHTML= categorias;
+
+}
+
+function mostrarPlatos(platos){
+    let categoriaActual=[];
+    platos.forEach(plato=>{
+        if(!categoriaActual[plato.categoria]){
+            categoriaActual[plato.categoria]="";
+        }
+        categoriaActual[plato.categoria]+=`
+            <div class="container">
+                <div class="row">
+                    <article class="card">
+                        <img src="../assets/${plato.id}.webp" class="card-img-top" alt="Ilustracion de ${plato.nombre}">
+                        <div class="card-body">
+                            <h5 class="card-title">${plato.nombre}</h5>
+                            <p class="card-text">${plato.descripcion}<br>
+                            Precio: $${plato.precio}</p>
+                            <button type="button" class="btn btn-primary" value="${plato.id}">Agregar al Carrito</button>
+                        </div>
+                    </article>
+                </div> 
+            </div>       
+        `;
+        
+    })
+    for(const categoria in categoriaActual){
+        /** para crear la categoria */
+        const seccion=document.getElementById(categoria);
+        seccion.innerHTML=categoriaActual[categoria];
+        /** para agregar evento a cada boton */
+        categoriaActual[categoria].querySelectorAll('button').forEach(button => {
+            button.addEventListener('click', () => {
+                agregarAlCarrito(button.value);
+                console.log(carrito)
+            });
+        })
+    }
+}
+
+/*function filtrarPlatos(platos){
+    const buscador= document.getElementById("buscador");
+    buscador.addEventListener("input",(e)=>{
+        const busca=e.target.value.toLowerCase();
+        const platosFiltrados= platos.filter(plato =>
+            plato.nombre.toLowerCase().includes(busca) || plato.descripcion.toLowerCase().includes(busca)
+        );
+        mostrarPlatos(platosFiltrados);
+    })
+}
+*/
+
+let carrito=[];
+
+function agregarAlCarrito(plato){
+    carrito.push(plato);
+}
